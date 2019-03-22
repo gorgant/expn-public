3a46cf137b0c41e57fed449cb872c50ec28416e6
Gorgant CX1 <gorgant@gorgant.com>
2019-03-22 18:11:07 +0800

    implement nav functionality

--- src/app/content/modules/content-routing.module.ts
+++ src/app/content/modules/content-routing.module.ts
@@ -1,11 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
+import { AboutComponent } from '../components/about/about.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { ServicesComponent } from '../components/services/services.component';
+import { PodcastComponent } from '../components/podcast/podcast.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'services',
+    component: ServicesComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'podcast',
+    component: PodcastComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
   },
   {
     path: '',
--- src/app/content/modules/content.module.ts
+++ src/app/content/modules/content.module.ts
@@ -3,9 +3,14 @@ import { NgModule } from '@angular/core';
 import { ContentRoutingModule } from './content-routing.module';
 import { HomeComponent } from '../components/home/home.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { AboutComponent } from '../components/about/about.component';
+import { ServicesComponent } from '../components/services/services.component';
+import { BlogComponent } from '../components/blog/blog.component';
+import { PodcastComponent } from '../components/podcast/podcast.component';
+import { ContactComponent } from '../components/contact/contact.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, AboutComponent, ServicesComponent, BlogComponent, PodcastComponent, ContactComponent],
   imports: [
     SharedModule,
     ContentRoutingModule
