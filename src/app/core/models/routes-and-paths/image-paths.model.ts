0bca4bd60c2ac12f76fe7c552097fb00c1f00abe
Gorgant CX1 <gorgant@gorgant.com>
2019-04-15 12:53:22 +0800

    implement real testamonials

--- src/app/core/models/routes-and-paths/image-paths.model.ts
+++ src/app/core/models/routes-and-paths/image-paths.model.ts
@@ -7,3 +7,18 @@ export enum ImagePaths {
   POST_HERO = 'assets/images/background/post_hero_background_explearning_logo.jpg',
   MD_AVATAR = 'assets/images/avatar/mary_daphne_explearning_avatar.png'
 }
+
+export enum TestamonialImagePaths {
+  AKIEM = 'assets/images/testamonials/akie_square_thumb@150.jpg',
+  ANNC = 'assets/images/testamonials/ann_square_thumb@150.jpg',
+  ASHINV = 'assets/images/testamonials/ashin_square_thumb@150.jpg',
+  BERNABEB = 'assets/images/testamonials/bernabe_square_thumb@150.jpg',
+  EVAS = 'assets/images/testamonials/eva_square_thumb@150.jpg',
+  FRANCIOS_XAVIERL = 'assets/images/testamonials/francios-xavier_square_thumb@150.jpg',
+  HYEWONK = 'assets/images/testamonials/hyewon_square_thumb@150.jpg',
+  KARINW = 'assets/images/testamonials/karin_thumb@150.jpg',
+  MARCELAC = 'assets/images/testamonials/marcela_square_thumb@150.jpg',
+  MEGUMIM = 'assets/images/testamonials/megumi_square_thumb@150.jpg',
+  YUS = 'assets/images/testamonials/yu_square_thumb@150.jpg',
+  UTKUB = 'assets/images/testamonials/utku_square_thumb@150.jpg',
+}
