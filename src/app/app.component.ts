466ddbb8d71aab673e69eaf5eb90f00fe05d3626
Gorgant CX1 <gorgant@gorgant.com>
2019-07-02 22:45:04 +0800

    upgrade to 1.6.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,7 +27,7 @@ import { AnalyticsService } from './core/services/analytics/analytics.service';
 })
 export class AppComponent implements OnInit {
   title = 'Explearning - Communicate With Clarity';
-  appVersion = '1.6.3';
+  appVersion = '1.6.4';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
