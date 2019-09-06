4bcaa7c9578bf04dd6ff4c7c05c64439dba500b9
Gorgant CX1 <gorgant@gorgant.com>
2019-09-06 13:39:23 +0300

    update meta tags

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,9 @@ import { AuthService } from './core/services/auth.service';
 import { Store } from '@ngrx/store';
 import { RootStoreState, UserStoreSelectors, AuthStoreSelectors, AuthStoreActions, UserStoreActions } from './root-store';
 import { withLatestFrom, map, takeWhile } from 'rxjs/operators';
-import { Meta } from '@angular/platform-browser';
 import { AngularFirestore } from '@angular/fire/firestore';
-import { AnalyticsService } from './core/services/analytics/analytics.service';
 import { ProductStrings } from 'shared-models/products/product-strings.model';
 import { Product } from 'shared-models/products/product.model';
-import { PublicImagePaths } from 'shared-models/routes-and-paths/image-paths.model';
 import { metaTagDefaults } from 'shared-models/analytics/metatags.model';
 
 @Component({
@@ -30,9 +27,7 @@ export class AppComponent implements OnInit {
   constructor(
     private uiService: UiService,
     private authService: AuthService,
-    private analyticsService: AnalyticsService,
     private store$: Store<RootStoreState.State>,
-    private metaTagService: Meta,
     private afs: AngularFirestore
   ) {}
 
@@ -41,7 +36,6 @@ export class AppComponent implements OnInit {
     this.configureSideNav();
     this.configureAuthDetection();
     this.checkForOfflineProductData();
-    this.configSeoAndAnalytics();
     this.initializePublicUser();
   }
 
@@ -120,27 +114,4 @@ export class AppComponent implements OnInit {
       this.store$.dispatch(new UserStoreActions.SetCartData({productData}));
     }
   }
-
-  private configSeoAndAnalytics() {
-
-    const title = metaTagDefaults.explearningPublic.metaTagDefaultTitle;
-    // tslint:disable-next-line:max-line-length
-    const description = metaTagDefaults.explearningPublic.metaTagDefaultDescription;
-    const localImagePath = PublicImagePaths.HOME;
-
-    this.analyticsService.setSeoTags(title, description, localImagePath);
-
-    this.metaTagService.addTags([
-      // tslint:disable-next-line:max-line-length
-      { name: 'keywords', content: metaTagDefaults.explearningPublic.metaTagDefaultKeywords },
-      // tslint:disable-next-line:max-line-length
-      { name: 'description', content: description },
-      { name: 'author', content: metaTagDefaults.explearningPublic.metaTagAuthor },
-      { name: 'twitter:site', content: metaTagDefaults.explearningPublic.metaTagTwitterHandle }, // Twitter analytics
-      { name: 'twitter:card', content: metaTagDefaults.explearningPublic.metaTagTwitterCardType }, // Twitter card display properties
-      { property: 'og:site_name', content: metaTagDefaults.explearningPublic.metaTagSiteName },
-      { property: 'fb:app_id', content: metaTagDefaults.explearningPublic.metaTagFbAppId }, // Facebook analytics
-    ]);
-  }
-
 }
