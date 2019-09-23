00f4ba8260070d532b29142b906c5808405b3a05
Gorgant CX1 <gorgant@gorgant.com>
2019-09-23 09:42:52 +0800

    update to 1.8.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,7 +17,7 @@ import { metaTagDefaults } from 'shared-models/analytics/metatags.model';
 })
 export class AppComponent implements OnInit {
   title = metaTagDefaults.explearningPublic.metaTagDefaultTitle;
-  appVersion = '1.8.2';
+  appVersion = '1.8.3';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
