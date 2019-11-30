b0fb17bde9fa8ef94d0bd6bcda300585d1acdc1d
Gorgant CX1 <gorgant@gorgant.com>
2019-11-30 15:32:36 +0800

    update to 1.8.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ import { DownloadPromoComponent } from './shared/components/email-collection/dow
 })
 export class AppComponent implements OnInit {
   title = metaTagDefaults.explearningPublic.metaTagDefaultTitle;
-  appVersion = '1.8.5';
+  appVersion = '1.8.6';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
