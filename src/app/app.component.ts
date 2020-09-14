bdb17769a3e4aef6341e5a6a3f25a4935a4ff3e6
Gorgant CX1 <gorgant@gorgant.com>
2020-09-14 08:06:38 -0400

    upgrade to 2.0.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ import { PublicAppRoutes } from 'shared-models/routes-and-paths/app-routes.model
 })
 export class AppComponent implements OnInit {
   title = metaTagDefaults.explearningPublic.metaTagDefaultTitle;
-  appVersion = '1.9.9';
+  appVersion = '2.0.0';
 
   private userAuthenticationRequested: boolean;
   private userLoaded: boolean;
